dcc7f4842425053f6bd37a2a88e25df43de5e13a
Kevin Marques <145597366+Kevinnmpl@users.noreply.github.com>
2024-10-05 11:52:43 -0300

    Delete AtorRepository.java

--- AtorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fujideia.iesp.tecback.repository;
-
-import br.com.fujideia.iesp.tecback.model.Ator;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AtorRepository extends JpaRepository<Ator, Long> {
-}
